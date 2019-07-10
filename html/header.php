0c697f42ebf3b9a4fa01a30eba2838f1303e3579
CemAktasGit <52286454+CemAktasGit@users.noreply.github.com>
2019-07-10 09:47:19 +0200

    Add files via upload

--- html/header.php
+++ html/header.php
@@ -8,43 +8,47 @@
       
       <header class="container0">
         <div class="box1">
-            <img class="logo" src="../img/logo2.png" alt="logo">
-            <a href="../index.php">Random School</a>
-        </div>
-        <div class="box1b">
-            <ul class="nav-links">
-                <li>    <a href="./courses.php">Courses</a> </li>
-                <li>    <a href="./registration.php">Registration</a> </li>
-                <li>    <a href="./contact.php">Contact Us</a> </li>
-            </ul> 
-        </div>
-            
-          <div class="login">
             <?php
                 if (isset($_SESSION['username'])){
                         echo "<p class='logintext'>Logged In</p>";
                         
-                        echo     "<div class=logout> ";
-                        echo    "<form action='../connect/logout.php' method='post'>";
+                        echo "<div class=logout> ";
+                        echo    "<form action='./connect/logout.php' method='post'>";
                         echo    "<button type='submit' name='button-logout' >Logout</button>";
                         echo "</div>";
                     }
                     else{
-                        echo "<form action='../connect/login.php' method='post'>";
+                        echo "<form action='./connect/login.php' method='post'>";
+                        echo "<div class=entry>";
+                        echo "<div class=entry-field1>";
                         echo "<input type='text' name='username' placeholder='Username'>";
+                        echo "</div>";
+                        echo "<div class=entry-field1>";
                         echo "<input type='password' name='password' placeholder='Password'>";
-                        echo "<br>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "<div class=entry-field1>";
                         echo "<button type='submit' name='button-login' >Login</button>";
+                        echo "</div>";
                         echo "</form>";
                         
-                        echo    "<div class=logout> ";
+                        echo "<div class=signup> ";
                         echo    "<a href='./html/signup.php'>SignUp</a>";
                         echo "</div>";
                         
                     }
                 
             ?>
-          </div>    
+          </div>     
+        <div class="box2">
+            <ul class="nav-links">
+                <li>    <a href="../index.php">Random School</a> </li>
+                <li>    <a href="./courses.php">Courses</a> </li>
+                <li>    <a href="./registration.php">Registration</a> </li>
+                <li>    <a href="./contact.php">Contact</a> </li>
+            </ul> 
+        </div>
+            
       </header>    
 
 <!----------------------------END-NAVIGATE-------------------------------------------------->  
